46bcc6b5606c298512391856b3462cf6cf71bc22
與田　レオ <136808619+Yoda-reo@users.noreply.github.com>
2026-02-05 15:47:15 +0900

    test_cases.pyの更新を行いました

--- test_cases.py
+++ test_cases.py
@@ -5,17 +5,24 @@ from calc_mul import calc
 
 # Run with testrunner so needs to be in file test_
 
-class TestCalc (unittest.TestCase):
+class TestCalc(unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    def test_sample1 (self):
+        self.assertEqual(6, calc(2, 3))
+        self.assertEqual(20, calc(5, 4))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    def test_sample2 (self):
+        self.assertEqual(-1, calc(0, 5))
+        self.assertEqual(-1, calc(7, 0))
+        self.assertEqual(-1, calc(0, 0))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+    def test_sample3 (self):
+        self.assertEqual(-1 ,calc(-2, 3))
+        self.assertEqual(-1, calc(2, -3))
+        self.assertEqual(-1, calc(-2, -3))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+    def test_sample4 (self):
+        self.assertEqual(1, calc(1, 1))
+        self.assertEqual(-1, calc(-1, 1))
+        self.assertEqual(-1, calc(1, -1))
+        self.assertEqual(-1, calc(-1, -1))
